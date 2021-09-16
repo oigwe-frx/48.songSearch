afe7a28056a028b16636760309df821a61684504
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-16 00:23:10 -0400

    Add files via upload

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './Track.css';
+
+class Track extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
+
+    addTrack(event){
+        return this.props.action(this.props.info);
+    }
+
+    removeTrack(event){
+        this.props.action(this.props.info);
+    }
+
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{this.props.info.name}</h3>
+                    <p>{this.props.info.artists} | {this.props.info.album}</p>
+                </div>
+                {
+                    this.props.isRemoval === false ? <button className="Track-action" onClick={this.addTrack}>+</button> : <button className="Track-action" onClick={this.removeTrack}>-</button>
+                }
+            </div>
+        )
+    }
+}
+
+export default Track;
